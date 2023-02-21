60d5323baad6ed1fbab498f920dc4dba8458f1ce
LeahKamau <kamauleah35@gmail.com>
2023-02-21 11:23:56 -0800

    correction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,8 +1,8 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers
- * 			using selection sort
+ * selection_sort - sorts using selection sort
+ *
  * @array: array to be sorted
  * @size: size of array to be sorted
  * Return: nothing
@@ -11,11 +11,16 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
+
+	if (size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		size_t min_idx = i;
 		int temp;
-		for(j = i + 1; j < size; j++)
+
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
