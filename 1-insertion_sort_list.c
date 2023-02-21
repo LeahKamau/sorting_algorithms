0bb4b6477595dfadf421e9c348631d884acb618e
LeahKamau <kamauleah35@gmail.com>
2023-02-21 04:05:55 -0800

    hot fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = *list, *temp;
 
-	if (*list == NULL || current->next == NULL)
+	if (*list == NULL || list  == NULL)
 		return;
 
 	while (current)
