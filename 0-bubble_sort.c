d077b0f416d69b5fc1fd537f5e6bdb2cdf8cc4ca
LeahKamau <kamauleah35@gmail.com>
2023-02-21 03:05:43 -0800

    hot fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,6 +8,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
@@ -15,6 +16,7 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 			{
 				int temp = array[j];
+
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 			}
