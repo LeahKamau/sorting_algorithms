256e8bdf064d3cfdeecdabd34c2f9a4c72f85f7b
LeahKamau <kamauleah35@gmail.com>
2023-02-21 11:32:51 -0800

    correction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,10 +25,13 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
-		temp = array[min_idx];
-		array[min_idx] = array[i];
-		array[i] = temp;
+		if (min_idx != i)
+		{
+			temp = array[min_idx];
+			array[min_idx] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
 
-		print_array(array, size);
 	}
 }
